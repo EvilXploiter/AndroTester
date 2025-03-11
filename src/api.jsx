e46d833eea0215f34d2da90020f4d435a2499213
EvilXploiter <165995344+EvilXploiter@users.noreply.github.com>
2025-03-11 17:52:41 +0530

    Delete src directory

--- src/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = "http://localhost:8080";
-
-export const login = async (username, password) => {
-  const res = await axios.post(`${API_URL}/login`, { username, password });
-  return res.data.token;
-};
